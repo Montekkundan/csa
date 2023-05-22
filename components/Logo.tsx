f56527bbdb64cb5a7962e07b434cc7b60f56db7c
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-05-21 22:17:46 -0700

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ function Logo(props:any) {
     const {renderDefault, title} = props;
   return (
     <div className="flex items-center space-x-2">
-         <Image className="rounded-full object-cover" width={50} height={50} src={"/../public/csalogo.png"} alt={"csa logo"}/>
+         <Image className="rounded-full object-cover" width={50} height={50} src='/csalogo.png' alt={"csa logo"}/>
          <>{renderDefault(props)}</>
     </div>
   )
